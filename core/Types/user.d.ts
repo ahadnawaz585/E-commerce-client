d27f20bff30fd829c1382232a59d8d2a01fb0e6d
Ahad <ahadnawaz585@gmail.com>
2024-04-28 17:31:05 +0500

    added enums

--- core/Types/user.d.ts
+++ core/Types/user.d.ts
@@ -1,9 +1,11 @@
 import { Role } from "../enums/role";
 export interface User {
-  id: string;
+  id?: string;
   username: string;
   password: string;
   email:string;
+  rememberMe?:boolean;
+  otp?: number
   role: Role;
   createdAt?: Date;
   updatedAt?: Date;
